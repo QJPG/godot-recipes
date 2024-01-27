4a8f57f4af44a181a51a9cabe3c1f9de10c28f09
QJPG <53179496+QJPG@users.noreply.github.com>
2024-01-27 16:57:28 -0300

    changed reset transform to local

--- TransformInterpolator.gd
+++ TransformInterpolator.gd
@@ -2,15 +2,19 @@ class_name TransformInterpolator extends Node
 
 @export var from : Node3D : set = SetReset
 @export var to : Node3D
-@export var look_at_delay := 5.0
+@export var look_at_speed := 5.0
 @export var reset_delay := 1.0
 
 var reset_transform: Transform3D
 
 func SetReset(itemNode: Node3D) -> void:
 	from = itemNode
+	
+	if not from:
+		return
+	
 	await from.tree_entered
-	reset_transform = from.global_transform
+	reset_transform = from.transform
 
 func _ready():
 	pass
@@ -35,12 +39,9 @@ func Reset(interpolateTime := 0.5) -> void:
 	if not from:
 		return
 	
-	if not to:
-		return
-	
-	var x = from.global_transform.interpolate_with(reset_transform, reset_delay)
+	var x = from.transform.interpolate_with(reset_transform, reset_delay)
 	var t = from.create_tween() as Tween
-	t.tween_property(from, "global_transform", x, interpolateTime)
+	t.tween_property(from, "transform", x, interpolateTime)
 	t.set_ease(Tween.EASE_OUT_IN)
 	t.play()
 
@@ -52,5 +53,5 @@ func _process(delta):
 		return
 	
 	var x = from.global_transform.looking_at(to.global_position)
-	var y = from.global_transform.interpolate_with(x, look_at_delay * delta)
+	var y = from.global_transform.interpolate_with(x, look_at_speed * delta)
 	from.global_transform = y
